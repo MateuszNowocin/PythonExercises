85b46e8d0db347efef336d6634ce7af80acf5f34
Mateusz Nowocin <matimatima@gmail.com>
2024-06-27 12:37:30 +0200

    New programs

--- ProgramDoAnalizyPikseli.py
+++ ProgramDoAnalizyPikseli.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-
+#Wczytuje obraz z pliku i konwertuje go z BGR do RGB
 def load_image(file_name):
     try:
         img = cv2.imread(file_name)
@@ -14,7 +14,7 @@ def load_image(file_name):
         print(f"Nie udało się załadować pliku o nazwie {file_name}. Błąd: {str(e)}")
         return None
 
-
+# Oblicza statystyki kolorów dla podanego obrazu
 def calculate_color_statistics(img):
     total_pixels = img.shape[0] * img.shape[1]
     red_pixels = np.sum((img[:, :, 0] > img[:, :, 1]) & (img[:, :, 0] > img[:, :, 2]))
@@ -24,7 +24,7 @@ def calculate_color_statistics(img):
 
     return red_pixels, green_pixels, blue_pixels, gray_pixels, total_pixels
 
-
+#Wypisuje statystyki kolorów dla obrazu
 def print_statistics(file_name, red, green, blue, gray, total):
     print(f"Nazwa pliku obrazka: {file_name}")
     print(f"Liczba czerwonych pikseli: {red}")
@@ -33,28 +33,33 @@ def print_statistics(file_name, red, green, blue, gray, total):
     print(f"Liczba szarych pikseli: {gray}")
     print(f"Łączna liczba pikseli: {total}")
 
-
+#Tworzy i wyświetla wykres kołowy, przedstawiający udział kolorów w obraziku
 def plot_pie_chart(red, green, blue, gray):
     labels = ['Czerwone', 'Zielone', 'Niebieskie', 'Szare']
     sizes = [red, green, blue, gray]
     colors = ['red', 'green', 'blue', 'gray']
 
+    plt.figure()  # Utworzenie nowego wykresu
     plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
     plt.axis('equal')
     plt.title('Udział kolorów w obrazku')
-    plt.show()
+    plt.show(block=False)
 
-#program uruchamiamy z poziomu cmd, podajac dodatkowo nazwe pliku obrazka
+#Program w pętli, tak długo jak nie wpiszemy exit, nie zakończy się
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        file_name = sys.argv[1]
-    else:
-        file_name = input("Podaj nazwę pliku obrazka: ")
+    while True:
+        file_name = input("Podaj nazwę pliku obrazka (lub 'exit' aby zakończyć): ")
 
-    img = load_image(file_name)
+        if file_name.lower() == 'exit':
+            print("Koniec programu.")
+            break
 
-    if img is not None:
-        red, green, blue, gray, total = calculate_color_statistics(img)
-        print_statistics(file_name, red, green, blue, gray, total)
-        plot_pie_chart(red, green, blue, gray)
+        img = load_image(file_name)
+
+        if img is not None:
+            red, green, blue, gray, total = calculate_color_statistics(img)
+            print_statistics(file_name, red, green, blue, gray, total)
+            plot_pie_chart(red, green, blue, gray)
+
+        print("\n")  # Dodanie pustej linii dla lepszej czytelności
